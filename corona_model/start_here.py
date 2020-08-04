9817c8e0c420dd9cf37a40ad2b2d54ee21e0161a
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-08-04 17:11:31 -0400

    update

--- corona_model/start_here.py
+++ corona_model/start_here.py
@@ -479,9 +479,9 @@ def main():
                 configCopy[categoryKey][specificKey] = specificValue
         R0Count = 100 if index == 0 else (100 if index > 4 else 30)
         multiCounts =  50 if index == 0 else (50 if index > 4 else 5)
-        R0Count = 3#, 80
+        R0Count = 10#, 80
         multiCounts = 30
-        if index ==0:#in [5, 6, 7, 8, 9, 10, 11, 12, 13]: 
+        if index in [5, 6, 7, 8, 9, 10, 11, 12, 13]: 
             modelName=modelName+"_"+str(simulationGeneration)
             #model_framework.simpleCheck(configCopy, days=100, visuals=True, debug=False, modelName=modelName)
             #InfectedCountDict[modelName] = model_framework.multiSimulation(multiCounts, configCopy, days=100, debug=False, modelName=modelName) 
